1d0f85db3100cfd2eb92021f2c1f7aed3644e1f7
Ayana <mmrtenshi@gmail.com>
2026-04-25 21:21:06 +0600

    fix: add redirect to solve 404

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
+    // Это мы оставляем, чтобы билд не падал из-за типов
     ignoreBuildErrors: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
+  async redirects() {
+    return [
+      {
+        source: '/',           // Если пользователь зашел на корень сайта
+        destination: '/register', // Его перекинет сюда
+        permanent: true,       // Браузер запомнит этот редирект
+      },
+    ];
   },
 };
 
 export default nextConfig;
+
+
